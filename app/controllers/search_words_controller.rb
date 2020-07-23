b9e6786c51380843f8077d50435df7d270235b88
kosaito222 <kosaito222@gmail.com>
2020-07-23 20:01:27 +0900

    Update Standard-Search
    - キーワード検索機能
    - 入力の最小文字数制限
    - 検索結果数の取得
    - モーダルを一時的にコメントアウト
    -

--- app/controllers/search_words_controller.rb
+++ app/controllers/search_words_controller.rb
@@ -1,4 +1,10 @@
 class SearchWordsController < ApplicationController
   def index
+    # フォームからキーワードを受け取って楽天から検索して結果をもらう
+    if params[:keyword].present?
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+    else
+      puts "空白です。"
+    end
   end
 end
